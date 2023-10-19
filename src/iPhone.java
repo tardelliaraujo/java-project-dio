0056d5f5e76772bb09ff9ef4f1ebbe196220590e
tardelliaraujo <tardelli.blues@gmail.com>
2023-10-19 19:57:59 -0300

    first commit

---
+++ src/iPhone.java
@@ -0,0 +1,54 @@
+public class iPhone implements ReprodutorMusical, AparelhoTelefonico, NavegadorInternet {
+    private String url;
+
+    public iPhone() {
+
+    }
+
+    @Override
+    public void ligar(String numero) {
+
+    }
+
+    @Override
+    public void atender() {
+
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+
+    }
+
+    @Override
+    public void adicionarNovaAba(String url) {
+
+    }
+
+    @Override
+    public void atualizarPagina() {
+
+    }
+
+    @Override
+    public void tocar() {
+
+    }
+
+    @Override
+    public void pausar() {
+
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+
+    }
+
+    // implementação dos métodos das interfaces
+}
